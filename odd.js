ae2bf4dfd1066bd4d2b4def5fe43cc24a51fb1a8
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-29 20:23:56 +0600

    reverse loop add

---
+++ odd.js
@@ -0,0 +1,15 @@
+let sum = 0;
+let i = 81;
+while(i<=131){
+  sum+=i
+  i+=2
+}
+console.log(sum)
+
+let sums = 0
+let odd = 1
+while(odd<=11){
+  sums+=odd
+  odd+=2
+}
+console.log(sums)
---
+++ while2.js
@@ -0,0 +1,5 @@
+let count = 1
+while(count<=60){
+  console.log(count, 'I will invest at least 6 hrs every single day for next 60 days!')
+  count+=1
+}
