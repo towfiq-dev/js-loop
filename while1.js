f30915f08687bc80a5125f97d2f7882e5ebd4c7c
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-29 19:03:17 +0600

    js-loop-practices

---
+++ while1.js
@@ -0,0 +1,21 @@
+let count = 0;
+while(count<10){
+  console.log('Hello World')
+  count+=1
+}
+
+let num = 0;
+while(num<=10){
+  console.log(num)
+  num+=1
+}
+let nums = 1;
+while(nums<=20){
+  console.log(nums)
+  nums+=2
+}
+let score =10;
+while(score>=1){
+  console.log(score)
+score-=2
+}
