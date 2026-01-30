d39b8b76fc714e6f77fd81d3c166a1890bd5e88c
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-30 07:48:05 +0600

    odd-even added

---
+++ odds.js
@@ -0,0 +1,23 @@
+for(let i = 81; i<=131; i+=1){
+  if(i % 2==1){
+  console.log(i)
+  }
+}
+
+for(let is =21; is<=40; is+=1){
+  if(is%2==1){
+    console.log(is)
+  }
+}
+
+for(let x= 0; x<=10; x+=1){
+if(x % 2==0){
+  console.log(x)
+}
+}
+
+for(let z=65; z<=75; z+=1){
+  if(z%2==1){
+    console.log(z)
+  }
+}
